36bb2ad3671d191c784a6adc3cea7c3ecefa12f1
Benny <bmota023@student.wethinkcode.co.za>
2024-11-07 19:57:32 +0200

    refactor app.js

--- app.js
+++ app.js
@@ -1,97 +1,59 @@
-'use strict'
+'use strict';
 
-const ipAddressField = document.querySelector('.ipAddressField')
-const timezoneInput = document.querySelector('.timezoneInput')
-const countryLocationInput = document.querySelector('.locationInput')
-const ispInput = document.querySelector('.ispInput')
-const submitBtn = document.querySelector('.submit-btn')
-const inputField = document.querySelector('.input-field')
+const ipAddressField = document.querySelector('.ipAddressField');
+const timezoneInput = document.querySelector('.timezoneInput');
+const countryLocationInput = document.querySelector('.locationInput');
+const ispInput = document.querySelector('.ispInput');
+const submitBtn = document.querySelector('.submit-btn');
+const inputField = document.querySelector('.input-field');
 
-
-let map = L.map('map').setView([51.505, -0.09], 13)
+let map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map)
-
-
-let ipAddress
-let randomIP = ''
-let timeZone
-let countryLocation
-let cityLocation
-let postalCode
-let isp
-let lat
-let lng
-
-let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.API_KEY}`
-fetch(url)
-  .then((response) => response.json())
-  .then((response) => {
-    ipAddress = response.ip
-    timeZone = response.time_zone.offset
-    countryLocation = response.country_name
-    cityLocation = response.city
-    postalCode = response.zipcode
-    isp = response.isp
-    lat = response.latitude
-    lng = response.longitude
-
-    ipAddressField.innerHTML = ipAddress
-    timezoneInput.innerHTML = ` UTC ${timeZone}`
-    countryLocationInput.innerHTML = `${countryLocation}, ${cityLocation} ${postalCode}`
-    ispInput.innerHTML = isp
-    mapLocation(lat, lng)
-  }).catch(error => console.log(error))
+}).addTo(map);
 
 const mapLocation = (lat, lng) => {
   var markerIcon = L.icon({
     iconUrl: 'images/icon-location.svg',
-
     iconSize: [46, 56],
     iconAnchor: [23, 55],
-  })
-  map.setView([lat, lng], 17)
+  });
+  map.setView([lat, lng], 17);
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: false,
-  }).addTo(map)
+  L.marker([lat, lng], { icon: markerIcon }).addTo(map);
+};
 
-  L.marker([lat, lng], { icon: markerIcon }).addTo(map)
-}
+const fetchLocationData = (ip) => {
+  const url = `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.API_KEY}&ip=${ip}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((response) => {
+      const { ip, time_zone, country_name, city, zipcode, isp, latitude, longitude } = response;
 
+      ipAddressField.innerHTML = ip;
+      timezoneInput.innerHTML = `UTC ${time_zone.offset}`;
+      countryLocationInput.innerHTML = `${country_name}, ${city} ${zipcode}`;
+      ispInput.innerHTML = isp;
+      mapLocation(latitude, longitude);
+    })
+    .catch((error) => console.error('Error fetching location data:', error));
+};
+
+fetchLocationData('');
 
 submitBtn.addEventListener('click', (event) => {
-  event.preventDefault()
+  event.preventDefault();
+  const ip = inputField.value;
+
   if (
-    inputField.value.match(
-      /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/,
+    ip.match(
+      /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
     )
   ) {
-    randomIP = inputField.value
-    url = `https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}=` + randomIP
-    fetch(url)
-      .then((response) => response.json())
-      .then((response) => {
-        ipAddress = response.ip
-        timeZone = response.time_zone.offset
-        countryLocation = response.country_name
-        cityLocation = response.city
-        postalCode = response.zipcode
-        isp = response.isp
-        lat = response.latitude
-        lng = response.longitude
-
-        ipAddressField.innerHTML = ipAddress
-        timezoneInput.innerHTML = ` UTC ${timeZone}`
-        countryLocationInput.innerHTML = `${countryLocation}, ${cityLocation} ${postalCode}`
-        ispInput.innerHTML = isp
-        mapLocation(lat, lng)
-      }).catch(error => console.log(error))
+    fetchLocationData(ip);
   } else {
-    alert('You have entered an invalid IP address!')
-    return false
+    alert('You have entered an invalid IP address!');
   }
-})
+});
